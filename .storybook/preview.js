1ffa9e46e9d7b6aae0ea24820d78850e3ad37e2a
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-12-12 22:39:31 +0100

    Configured storybook with Themes and globalStyles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,3 +7,40 @@ export const parameters = {
     },
   },
 };
+
+import { lightTheme, darkTheme } from "assets/styles/themes.tsx";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "assets/styles/globalStyle.tsx";
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      items: ["light", "dark"],
+      showName: true,
+    },
+  },
+};
+
+const MyThemes = {
+  dark: darkTheme,
+  light: lightTheme,
+};
+
+const getTheme = (themeName) => {
+  return MyThemes[themeName];
+};
+
+const withThemeProvider = (Story, context) => {
+  const theme = getTheme(context.globals.theme);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story {...context} />
+    </ThemeProvider>
+  );
+};
+export const decorators = [withThemeProvider];
