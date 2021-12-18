7c9d59b31bf45f379d5b0aeb71e9fe14722ca1c9
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-12-18 20:52:42 +0100

    Added ContentWrapper component

---
+++ src/components/molecules/ContentWrapper/ContentWrapper.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+interface IContentWrapper {
+  padding?: string;
+}
+
+export const ContentWrapper = styled.div<IContentWrapper>`
+  background-color: ${({ theme }) => theme.colors.bg.neutral};
+  padding: ${({ padding }) => padding || "32px"};
+  border-radius: 6px;
+`;
