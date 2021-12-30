9c6cc7c23660a7d50e85f23a5ab7af6afb56252c
Mieszko Kowalik <kowalik.mieszko@gmail.com>
2021-12-30 17:37:27 +0100

    Added useMediaQuery hook

---
+++ src/hooks/useMediaQuery.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const useMediaQuery = (query: string) => {
+  const [matches, setMatches] = useState(false);
+  const mediaQuery = window.matchMedia(query);
+  console.log(mediaQuery);
+  useEffect(() => {
+    if (mediaQuery.matches !== matches) {
+      setMatches(mediaQuery.matches);
+    }
+    const listener = () => {
+      console.log(mediaQuery.matches);
+      setMatches(mediaQuery.matches);
+    };
+    mediaQuery.addEventListener("change", listener);
+    return () => mediaQuery.removeEventListener("change", listener);
+  }, [mediaQuery, matches]);
+
+  return matches;
+};
+
+export default useMediaQuery;
